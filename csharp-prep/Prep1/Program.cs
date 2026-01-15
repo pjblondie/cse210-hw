1427cff0a35b90d029db106e8f6023656a84029c
pjblondie <ParkerJack@byui.edu>
2026-01-14 23:40:36 -0700

    finished Prep 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        {
+            Console.WriteLine("What is your first name?");
+            string first_name = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string last_name = Console.ReadLine();
+
+            Console.WriteLine($"The names {last_name}, {first_name} {last_name}");
+            
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Today!");
+        string x;
+        x = "you";
+        Console.WriteLine(x + 5);
     }
 }
+
+// Notes Start here! 
+
+
+
