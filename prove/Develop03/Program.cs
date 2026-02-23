1fc5c94d5ff1d38ced0de500e77e4ec769ca843e
pjblondie <ParkerJack@byui.edu>
2026-02-22 18:23:52 -0700

    Fully finished technically

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -12,7 +12,7 @@ class Program
         // Console.WriteLine($"{TestWord.SwapText()}");
 
         // Reference TestReference = new Reference("Nephi", 4, 10);
-        // Reference TestReference2 = new Reference("Nephi", 4, 10, 12);
+
 
         // Console.WriteLine($"{TestReference.GetDisplayText()}");
         // Console.WriteLine($"{TestReference2.GetDisplayText()}");
@@ -28,5 +28,23 @@ class Program
         // Console.WriteLine("--- AFTER HIDING 6 WORDS ---");
         // Console.WriteLine(TestScripture.DisplayScripture());
         // TESTS // 
+
+        Reference TestReference = new Reference("Nephi", 4, 10);
+        Scripture TestScripture = new Scripture(TestReference, "And it came to pass that I was constrained by the Spirit that I should kill Laban; but I said in my heart: Never at any time have I shed the blood of man. And I shrunk and would that I might not slay him.");
+        string UserInput = ("");
+        while (UserInput != "quit" && TestScripture.FullyHidden() == false)
+        {
+            Console.Clear();
+            Console.WriteLine(TestScripture.DisplayScripture());
+            UserInput = Console.ReadLine();
+            if (UserInput == "")
+            {
+                TestScripture.HideWords();
+            }
+        }
+        Console.Clear();
+        Console.WriteLine(TestScripture.DisplayScripture());
+    
+    
     }
 }
