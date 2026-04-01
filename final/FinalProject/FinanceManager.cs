c549ebcc7b5387b44134327b916229fbba56ef61
pjblondie <ParkerJack@byui.edu>
2026-04-01 01:11:10 -0600

    Fully working user input 1

--- final/FinalProject/FinanceManager.cs
+++ final/FinalProject/FinanceManager.cs
@@ -11,12 +11,22 @@ public class FinanceManager
 
     private List<Expense> _expenses;
 
+
+    public FinanceManager()
+    {
+        _categories = new List<Category>();
+        _subscription = new List<Subscription>();
+        _goals = new List<SavingsGoal>();
+        _expenses = new List<Expense>();
+    }
     public void DisplayMenu()
     {
-       bool running = true;
+        bool running = true;
         while (running == true)
         {
-
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("Personal Finance ");
+            Console.WriteLine("---------------------------------------------------");
             Console.WriteLine("------------------------------------");
             Console.WriteLine(" 1. Create a new budget Category");
             Console.WriteLine(" 2. Track a Subscription");
@@ -29,7 +39,7 @@ public class FinanceManager
             Console.WriteLine(" 9. Load");
             Console.WriteLine(" 10. Quit");
             Console.WriteLine("------------------------------------");
-
+            Console.Write("Record your response here: ");
             string userInput = Console.ReadLine();
 
             if (userInput == "1")
@@ -41,32 +51,48 @@ public class FinanceManager
                 Console.WriteLine("     1. A Spending Category (Food, Rent, Etc.)");
                 Console.WriteLine("     2. A Savings Category (Emergancy Fund, Etc.)");
                 Console.WriteLine("------------------------------------------------------");
-
+                Console.Write("Record your response here: ");
 
                 string categoryType = Console.ReadLine();
-            
-                        // 2. The Shared Questions
+
+                // 2. The Shared Questions
                 Console.Write("What is the name of the category? ");
                 string name = Console.ReadLine();
 
                 Console.Write("What is a short description? ");
                 string desc = Console.ReadLine();
 
-                Console.Write("What is the monthly limit/goal amount? ");
+                Console.Write("What is the monthly limit amount? ");
                 double limit = double.Parse(Console.ReadLine());
 
                 // 3. The Logic Branch
                 if (categoryType == "1")
                 {
-            
+
                     _categories.Add(new SpendingCategory(name, desc, limit));
+                    Console.Write("Saving your category...");
+                    Spinner();
+                    Console.WriteLine("");
+                    Console.WriteLine("------------------------------------------------------");
                     Console.WriteLine($"Success! '{name}' added as a Spending Category.");
+                    Console.WriteLine("------------------------------------------------------");
+                    Console.Write("     Please press enter to return to menu: ");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
                 else if (categoryType == "2")
                 {
-        
+
                     _categories.Add(new SavingCategory(name, desc, limit));
+                    Console.Write("Saving your category...");
+                    Spinner();
+                    Console.WriteLine("");
+                    Console.WriteLine("------------------------------------------------------");
                     Console.WriteLine($"Success! '{name}' added as a Saving Category.");
+                    Console.WriteLine("------------------------------------------------------");
+                    Console.WriteLine("     Please press enter to return to menu: ");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
                 else
                 {
@@ -110,7 +136,9 @@ public class FinanceManager
             {
                 // add auto save the file to Finance.txt
                 Console.Clear();
+                Console.WriteLine("----------------------------------------------------------------------------------");
                 Console.WriteLine("Thank you for using the personal finance app have a wonderful rest of your day!");
+                Console.WriteLine("----------------------------------------------------------------------------------");
                 running = false;
             }
             else
@@ -129,5 +157,20 @@ public class FinanceManager
     {
         
     }
-
+        public void Spinner()
+    {
+        List<string> spinlist = new List<string> { "/", "-", "\\", "|", "/", };
+        int spinTime = 0;
+        while (spinTime < 15)
+        {
+            foreach (string spin in spinlist)
+            {
+                Console.Write($"{spin}");
+                Thread.Sleep(400);
+                Console.Write("\b \b");
+                spinTime += 1;
+            }
+        }
+        Console.WriteLine("");
+    }
 }
--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -5,16 +5,20 @@ class Program
     static void Main(string[] args)
     {
 
-        
+        FinanceManager Finance1 = new FinanceManager();
         Console.Clear();
         Console.WriteLine("---------------------------------------------------");
         Console.WriteLine("Welcome to your personalized Finace Application!");
         Console.WriteLine("---------------------------------------------------");
-        Thread.Sleep(1000);
+        Console.Write("     Please press enter to continue: ");
+        Console.ReadLine();
         Console.Clear();
+        Console.Write("Preparing your finances, please wait...");
+        Finance1.Spinner();
         Console.WriteLine("-------------------------------------------------------");
         Console.WriteLine("To begin please select one of the following options: ");
         Console.WriteLine("-------------------------------------------------------");
-        
+
+        Finance1.DisplayMenu();
     }
 }
